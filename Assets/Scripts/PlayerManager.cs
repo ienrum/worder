e6ce555d361ea872808305e0a87e967394eb89a0
ienrum <azang0506@gmail.com>
2023-09-07 18:53:34 +0900

    feat: 플레이어 키보드 조작 수행 함수 작성

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,10 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    const int MAX_WORD_SIZE = 25;
+    public string answerWord = "";
+    private string playerInput = "";
+
+    public string PlayerInput
+    {
+        get { return playerInput; }
+    }
+        
+    private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     void Start()
     {
 
@@ -13,6 +24,66 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isKeyOf(getKeyToString(), "Return"))
+        {
+            answerWord = playerInput;
+            playerInput = "";
+            Debug.Log(answerWord);
+        }
+        else if (isKeyInAlphabet(getKeyToString()))
+        {
+            if (playerInput.Length < MAX_WORD_SIZE)
+            {
+                playerInput += getKeyToString();
+            }
+            
+        }
+        else if (isKeyOf(getKeyToString(), "Backspace"))
+        {
+            playerInput = delLastKey(playerInput);
+        }
+    }
+    private string delLastKey(string input)
+    {
+        string result = "";
+        if (input.Length > 0)
+        {
+            result = input.Substring(0, playerInput.Length - 1);
+        }
+        return result;
+    }
 
+    private bool isKeyInAlphabet(string input)
+    {
+        if (input.Count() != 1)
+        {
+            return false;
+        }
+        foreach(char c in alphabet)
+        {
+            if(input.Contains(c)) return true;
+        }
+        return false;
+    }
+
+    private bool isKeyOf(string input,string target)
+    {
+        if(input == target)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private string getKeyToString()
+    {
+        foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                return keyCode.ToString();
+            }
+        }
+        return "";
     }
 }
