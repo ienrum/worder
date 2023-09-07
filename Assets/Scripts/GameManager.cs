e9997016291c46e892c557e875bd2d4d8c338dc1
ienrum <azang0506@gmail.com>
2023-09-07 18:52:50 +0900

    feat: shuffle 오류 수정, ui 와 연동, 함수형 리팩토링 수행

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,48 +2,78 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     private List<string> wordDictionary = new List<string>() {"power","coffee","function","bug","man"};
+    string shuffledWords = "";
+
+    public List<Transform> boxUiList;
+    public Transform hintUI;
+    private PlayerManager playerManager;
     // Start is called before the first frame update
     void Start()
     {
+        List<char> chars;
+        chars = stringToCharList(String.Join("", wordDictionary));
 
-        List<char> chars = new List<char>();
-        chars.AddRange(String.Join(' ',wordDictionary));
+        List<char> shuffledCharList = ShuffleList(chars,100);
+        shuffledWords = charListToString(shuffledCharList);
 
-        Debug.Log(new string(chars.ToArray()));
+        playerManager = FindAnyObjectByType<PlayerManager>();
 
-        string shuffledWordList = new string(Shuffle(chars).ToArray());
-
-        Debug.Log( shuffledWordList);
+        hintUI.GetComponent<TextMeshProUGUI>().text = shuffledWords;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateBoxUIList(playerManager.PlayerInput,boxUiList);
+    }
+    private void updateBoxUIList(string input,List<Transform> list)
+    {
+        int n = list.Count();
+        for(int i=0;i<n;i++)
+        {
+            list[i].GetChild(0).GetComponent<TextMeshProUGUI>().text = i < input.Length ? input[i].ToString() : "";
+        }
     }
 
-    private List<T> Shuffle<T>(List<T> list)
+    private bool IsCorrectWord(string word, List<string> wordDictionary)
+    {
+        return wordDictionary.Contains(word.ToLower());
+    }
+
+    private List<T> ShuffleList<T>(List<T> list,int shuffleCount)
     {
         int n = list.Count;
-        List<T> result = new List<T>(new T[n]) ;
-        while (n > 1)
+        List<T> result = list.Select(t => t).ToList();
+        while (shuffleCount > 1)
         {
-            n--;
-            int k = UnityEngine.Random.Range(0, n);
-            Debug.Log(n+" "+k);
-            T temp = list[k];
-            list[k] = temp;
+            shuffleCount--;
+
+            int idx = shuffleCount % n;
+            int k = UnityEngine.Random.Range(0, shuffleCount %n);
+
+            T temp = result[idx];
+            result[idx] = result[k];
             result[k] = temp;
         }
         return result;
     }
-    private bool IsWord(string word,List<string> wordDictionary)
-    { 
-        return wordDictionary.Contains(word.ToLower());
+    private string charListToString(List<char> chars)
+    {
+        return new string(chars.ToArray());
+    }
+
+    private List<char> stringToCharList(string  word)
+    {
+        List<char> chars = new List<char>();
+        chars.AddRange(word);
+        return chars;
     }
 }
